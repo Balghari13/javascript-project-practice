c3b31165cdd3ef2a043b7ad4e33fc8aa347a3af3
Balghari <najmbalghari13@gmail.com>
2025-11-08 15:46:11 +0500

    implement local storage

--- 09.Focus Today/script.js
+++ 09.Focus Today/script.js
@@ -2,11 +2,24 @@ const check = document.querySelectorAll('.custom-checkbox')
 const errorLabel = document.querySelector('.error-label')
 const goalInput = document.querySelectorAll('.goal-input')
 const progressValue = document.querySelector('.progress-value')
-
-console.log(progressValue);
-
+const progressLabel = document.querySelector('.progress-label')
 
 
+
+const allGoal = JSON.parse(localStorage.getItem("allGoals"))||{}
+
+const allQuotes = [
+  'Raise the bar by completing your goals!',
+  'Well begun is half done!',
+  'Just a step away, keep going!',
+  'Whoa! You just completed all the goals, time for chill :D',
+]
+
+ goalCompleted = Object.values(allGoal).filter((goal)=>goal.completed).length;
+progressValue.style.width = `${goalCompleted/goalInput.length * 100}%`
+ progressValue.firstElementChild.innerText = `${goalCompleted}/${goalInput.length} completed`
+ progressLabel.innerText = `${allQuotes[goalCompleted]}`
+    console.log(goalCompleted);
 
 check.forEach((checked)=> {
   checked.addEventListener('click',()=>{
@@ -23,9 +36,28 @@ check.forEach((checked)=> {
 
   if(allGoals){
     checked.parentElement.classList.toggle('completed')
-    progressValue.style.width = '33.33%'
+    
+    const inputId = checked.nextElementSibling.id
+    allGoal[inputId].completed = !allGoal[inputId].completed
+  
+    goalCompleted = Object.values(allGoal).filter((goal)=>goal.completed).length;
+
+    
+     progressValue.style.width = `${goalCompleted/goalInput.length * 100}%`
+     
+    progressValue.firstElementChild.innerText = `${goalCompleted}/${goalInput.length} completed`
+    progressLabel.innerText = allQuotes[goalCompleted]
+    console.log(goalCompleted);
+    
+
+    localStorage.setItem('allGoals', JSON.stringify(allGoal))
+  
+   
   }else{
     errorLabel.style.display = 'block'
+    console.log(errorLabel.nextSibling);
+    
+    errorLabel.children[0].innerText = goalInput.length
   }
   
 
@@ -35,7 +67,45 @@ check.forEach((checked)=> {
 )
 
 goalInput.forEach((input)=>{
+
+if(allGoal[input.id]){
+input.value = allGoal[input.id].name
+
+if(allGoal[input.id].completed){
+    input.parentElement.classList.add('completed')
+  }
+}
+  
+
+  
+
   input.addEventListener('focus',()=>{
     errorLabel.style.display = 'none'
   })
+
+  input.addEventListener('input',()=>{
+
+    if(allGoal[input.id] && allGoal[input.id].completed){
+      input.value = allGoal[input.id].name
+      return
+    };
+    // console.log(input.id);
+    //  if (allGoals[input.id]) {
+    //   allGoals[input.id].name = input.value
+    // } else {
+    //   allGoals[input.id] = {
+    //     name: input.value,
+    //     completed: false,
+    //   }
+    // }
+    allGoal[input.id] = {
+      name: input.value,
+      completed: false
+    }
+    localStorage.setItem('allGoals', JSON.stringify(allGoal))
+    
+  })
+
 })
+
+
